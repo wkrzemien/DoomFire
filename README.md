9da644f799c9326b24ac6907474164757f00eb66
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2019-01-03 15:54:32 +0100

    Add readme and slightly modify the code

---
+++ README.md
@@ -0,0 +1,8 @@
+# Fire effects a la Doom 
+
+Simple implementation of fire effects as in the Doom game intro.
+The code is the adaptation of the javascript version by Fabien Sanglard. 
+Done only for fun and to learn a bit more about Python.
+
+See [this] (http://fabiensanglard.net/doom_fire_psx/)
+and [that] (https://github.com/fabiensanglard/DoomFirePSX/blob/master/flames.html)
--- run.py
+++ run.py
@@ -54,16 +54,8 @@ RGBS = [
   0xFF, 0xFF, 0xFF
 ]
 
-def group(lst, n):
-  """group([0,3,4,10,2,3], 2) => [(0,3), (4,10), (2,3)]
-
-  Group a list into consecutive n-tuples. Incomplete tuples are
-  discarded e.g.
-  >>> group(range(10), 3)
-  [(0, 1, 2), (3, 4, 5), (6, 7, 8)]
-  """
-  return zip(*[lst[i::n] for i in range(n)])
-
+def group(li, n):
+  return zip(*[li[i::n] for i in range(n)])
 
 def transformToRGB(lst):
   """
